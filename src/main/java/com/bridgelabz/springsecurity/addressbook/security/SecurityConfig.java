13fc43f84da712be243624d0c9f0bc98a5e7b46e
BL-maheswari <ipperu.maheswari@bridgelabz.com>
2026-01-22 11:24:39 +0530

    added userdetails  using the in memory

---
+++ src/main/java/com/bridgelabz/springsecurity/addressbook/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.springsecurity.addressbook.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+    public InMemoryUserDetailsManager configureMemoryUser() {
+
+        UserDetails user1 = User.withUsername("mahi")
+                .password("{noop}ma@123")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("admin")
+                .password("{bcrypt}admin123")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user3 = User.withUsername("test")
+                .password("{noop}test123")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
+    }
+}
+
+	
+
+
